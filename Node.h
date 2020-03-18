65d2d5ba5752791e4dd72b32d5360be81155d3ba
Adamek201 <61745745+Adamek201@users.noreply.github.com>
2020-03-18 13:46:00 +0100

    Add files via upload

---
+++ Node.h
@@ -0,0 +1,21 @@
+#pragma once
+
+template<typename Typ>
+class Node
+{
+public:
+	Typ data;   //zawartosc danego bloku
+	Node* next; // wskaznik na nastepny element
+
+	Node(const Typ data)
+	{
+		this->data = data;
+		next = NULL;
+	}
+
+	Node()
+	{
+		data = NULL;
+		next = NULL;
+	}
+};
--- main.cpp
+++ main.cpp
@@ -1,88 +1,36 @@
-#include "Quick.h"
-#include <iostream>
-#include <chrono>
-
-using namespace std;
-using namespace std::chrono;
-#define TypDanych int
-
-
-
-
-template<typename Typ>
-Typ* createTab(int size)
-{
-	Typ* Tab = new Typ[size];
-
-	return Tab;
-}
-
-template<typename Typ>
-void deleteTab(Typ* Tab)
-{
-	delete[] Tab;
-}
-
-
-template<typename Typ>
-void fill_randomTab(Typ* Tab,int size)
-{
-	for (int i = 0; i < size; i++)
-		Tab[i] = rand();
-}
-
-template<typename Typ>
-void check_sortedTab(Typ* Tab, int size)
-{
-
-	for (int i = 0; i < size - 1; i++)
-	{
-		if (Tab[i] > Tab[i + 1])
-		{
-			cout << "Array not sorted, something went wrong" << endl;
-			cin.get();;
-			exit(1);
-		}
-			
-	}
-}
-
-template<typename Typ>
-Typ* percentSort(Typ* Tab, double percent)
-{
-
-}
-
-
+#include "List.h"
 
 int main()
 {
-	int Size[5] = { 10000, 50000, 100000, 500000, 1000000 }; //rozmiary tablic
-	double Duration=0;
-	int number_ofTabs = 100;
-	double percentTab[8] = { 0, 25, 50, 75, 95, 99, 99.7, 100 }; //procent w jakim tablice sa poprzednie posortowane
-	for (int a : Size)
-	{
-		TypDanych* Array = createTab<TypDanych>(a);// glowna tablica
-		
-		
-		for (int i = 0; i < number_ofTabs; i++)
-		{
-			
-			fill_randomTab<TypDanych>(Array, a);
-			auto start = high_resolution_clock::now();
-			qckSort(Array, 0, a-1);
-			auto stop = high_resolution_clock::now();
-			check_sortedTab<TypDanych>(Array, a);
-			cout << i << '%'<< '\r';
-			
-			auto duration = duration_cast<milliseconds>(stop - start);
-			Duration += (double)duration.count();
-		}
+	List<int>* kontener = new List<int>();
 
-		cout << "Zostalo posortowane " << number_ofTabs << " tablic o rozmiarze " << a << " czas sortowania pojedynczej tablicy wynosi " << Duration/number_ofTabs << " ms"<< endl;
-		deleteTab<TypDanych>(Array);
-		
-	}
+	int a = 1;
+	int b = 2;
+	int c = 3;
+
+	kontener->pushBack(c);
+	kontener->pushBack(a);
+	kontener->pushBack(a);
+	kontener->remove(c);
+	kontener->pushBack(a);
+	kontener->pushFront(b);
+	kontener->pushBack(a);
+	kontener->insert(c, 1);
+	(*kontener)[0] = 5;
+	kontener->DisplayList();
+	cout << endl;
+
+	
+	cout << (*kontener)[2] << endl;
+
+	for (auto it = kontener->begin(); it != kontener->end(); ++it)
+		std::cout << *it << "  ";
+	
+	kontener->~List();
+	
+	cout << endl;
+
+
 	return 0;
+
 }
