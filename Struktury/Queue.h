c45c46ed042a107b40f702d2920136d580c1861d
Adamek201 <61745745+Adamek201@users.noreply.github.com>
2020-03-20 20:41:54 +0100

    Add files via upload

--- Struktury/Queue.h
+++ Struktury/Queue.h
@@ -9,6 +9,10 @@ class Queue
 private:
 	List<Typ>* queue;
 public:
+	Queue()
+	{
+		queue = new List<Typ>();
+	}
 	void enqueue(const Typ& newElement);
 	Typ dequeue();
 };
@@ -16,19 +20,19 @@ public:
 template<typename Typ>
 Typ Queue<Typ>::dequeue()
 {
-	Node<Typ>* tempNEXT, * temp;
-	Typ Data;
 	if (queue->head)
 	{
-		temp = queue->head;
-		Data = temp->data;
-		tempNEXT = queue->head->next;
-		queue->head = tempNEXT;
+		Node<Typ>* temp = queue->head;
+		Typ Data = temp->data;
+		queue->head = queue->head->next;
 		delete(temp);
-
-		return Data;
+			return Data;
+		
 	}
-	throw out_of_range("Kolejka nie istnieje");
+	else
+	{
+		throw out_of_range("Kolejka nie istnieje");
+	}
 }
 
 template<typename Typ>
