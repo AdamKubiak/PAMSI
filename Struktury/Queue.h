ec9cebbe0aa74ae6a9e112c533bd007bfdfedbe4
Adamek201 <61745745+Adamek201@users.noreply.github.com>
2020-03-20 21:14:56 +0100

    Add files via upload

--- Struktury/Queue.h
+++ Struktury/Queue.h
@@ -9,13 +9,30 @@ class Queue
 private:
 	List<Typ>* queue;
 public:
-	Queue()
-	{
-		queue = new List<Typ>();
-	}
+	Queue();
+	~Queue();
 	void enqueue(const Typ& newElement);
 	Typ dequeue();
 };
+
+template<typename Typ>
+Queue<Typ>::Queue()
+{
+	queue = new List<Typ>();
+}
+
+template<typename Typ>
+Queue<Typ>::~Queue()
+{
+	while (queue->head->next != nullptr) {
+		Node<Typ>* temp = queue->head;
+		queue->head = queue->head->next;
+		delete temp;
+	}
+	delete queue->head;
+	cout << "Kolejka zostala usunieta" << endl;
+}
+
 
 template<typename Typ>
 Typ Queue<Typ>::dequeue()
